918bc3dc6b351fd45435d7fb697439212e40f6f1
Sanjeet Rai <raisanjeet8896@gmail.com>
2024-02-20 10:30:41 +0530

    alternate method for todo list added

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -16,3 +16,34 @@ function addTask(){
 function removeTask(button){
     button.parentElement.remove();
 }
+/*
+Alternate to solve the problem as well
+function addTask() {
+  var taskInput = document.getElementById("taskInput");
+  var taskText = taskInput.value.trim();
+
+  if (taskText !== "") {
+    var taskList = document.getElementById("taskList");
+    var listItem = document.createElement("li");
+    var span = document.createElement("span");
+    span.textContent = taskText;
+    var removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    removeButton.onclick = function() {
+      removeTask(this);
+    };
+    listItem.appendChild(span);
+    listItem.appendChild(removeButton);
+    taskList.appendChild(listItem);
+    taskInput.value = "";
+  } else {
+    alert("Please enter a task!");
+  }
+}
+
+function removeTask(button) {
+  var listItem = button.parentNode;
+  var taskList = listItem.parentNode;
+  taskList.removeChild(listItem);
+}
+ */

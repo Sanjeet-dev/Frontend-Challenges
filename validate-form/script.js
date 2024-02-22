2ef66bc67796c1af339ba5a4e5178645c69df6a7
Sanjeet Rai <raisanjeet8896@gmail.com>
2024-02-22 20:34:28 +0530

    validate form

---
+++ validate-form/script.js
@@ -0,0 +1,23 @@
+//Write your javascript code here
+const nameText = document.querySelector("#name").value.trim();
+const mail = document.querySelector("#email").value.trim();
+const password = document.querySelector("#pass").value.trim();
+document.querySelector("#form").addEventListener("submit",function(event){
+    console.log("working form fine!!");
+    event.preventDefault();
+    let errmsg = "Fill the form correctly.";
+    let successmsg = "Form submitted successfully."
+    if(!nameText || /^[a-zA-Z ]+$/.test(nameText) === false){
+        alert(errmsg);
+        return;
+    }
+    if(!mail || !mail.endsWith("@codingninjas.com")){
+        alert(errmsg);
+        return;
+    }
+    if(!password || password.length < 8 || !password.includes("@")){
+        alert(errmsg);
+        return;
+    }
+    alert(successmsg);
+});

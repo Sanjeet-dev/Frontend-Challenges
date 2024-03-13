cb070fadbedd9e295dd670ea7ef72bc1943e6291
Sanjeet Rai <raisanjeet8896@gmail.com>
2024-03-13 10:41:08 +0530

    added ticking sound

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -1,26 +1,41 @@
 let count = 60;
 const min = document.querySelector("#min");
 const sec = document.querySelector("#sec");
+const start = document.querySelector("#startButton");
 const clear = document.querySelector("#clearButton");
+const tickSound = new Audio("tick.mp3");
 let timer = "";
-document.querySelector("button").addEventListener("click",()=>{
-    min.textContent = "01";
-   timer = setInterval(function() {
-        min.textContent = "00";
-        count--;
-        console.log(count);
-        sec.textContent=count;
-        if (count === 0) {
+
+
+function counter(){
+  min.textContent = "00";
+        if(count>0){
+            count--;
+            console.log(count);
+            sec.textContent=count;
+        }
+        
+        if (!count) {
           clearInterval(timer);
           sec.textContent="00";
           console.log("Time's up!");
         }
-      }, 1000);
+}
+
+start.addEventListener("click",()=>{
+  clearInterval(timer);
+  count=60;
+  min.textContent = "01";
+  tickSound.play();
+  timer = setInterval(counter, 1000);
 });
 
 clear.addEventListener("click", function(){
     clearInterval(timer);
     count=60;
-    min.textContent="01";
+    tickSound.pause();
+    tickSound.currentTime=0;
+    counter();
+    min.textContent="00";
     sec.textContent = "00";
 })
